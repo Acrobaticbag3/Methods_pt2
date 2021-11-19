ce5443f62dc54f44e76ff7170c1a2e400069cf29
Josund04 <89245272+Josund04@users.noreply.github.com>
2021-11-19 14:13:14 +0100

    Learning to do stuff with methods

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace More_about_methods {
+    class Program {
+        static void Main(string[] args) {
+            double b = 7;
+            double a;
+
+            Console.WriteLine("Input any number: "); 
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your number: " + a + " To the power of 7 is: " + Pow( a, b));             
+
+            Console.ReadKey();
+        }
+        
+        static double Pow( double a, double b) { 
+            return Math.Pow(a , b);
+        }
+    }
+}
